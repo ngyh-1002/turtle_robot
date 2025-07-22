28c7468436e50f131af31170f2ae752f87966ee2
ngyh-1002 <01049200297aa@gmail.com>
2025-07-22 11:13:21 +0900

    거리계산기능 추가
    함수로 구현

--- turtle_robot.py
+++ turtle_robot.py
@@ -1,8 +1,10 @@
 import turtle
+import math
 
 # 스크린 생성
 s = turtle.getscreen()
 s.setup(width=900, height=900)
+
 #거북이 변수 지정
 t = turtle.Turtle()
 t.shapesize(2,2,2)
@@ -10,11 +12,13 @@ t.shape('turtle')
 t.color("green", "blue")
 t.pensize(5)
 
+#출발지점 표시
 t.penup()
 t.goto(-350,-375)
 t.pendown()
 t.circle(50)
 
+#도착지점 표시
 t.penup()
 t.goto(325,325)
 t.pendown()
@@ -22,6 +26,7 @@ for i in range(4):
     t.fd(100)
     t.lt(90)
 
+# 장애물 지정
 t.penup()
 t.goto(0,0)
 t.pendown()
@@ -29,11 +34,23 @@ t.pendown()
 for i in range(3):
     t.fd(100)
     t.lt(120)
-    
-t.penup()
-t.goto(-350,-350)
-t.pendown()
-t.goto(-50, -50)
-t.lt(60)
-t.fd(200)
-t.goto(350, 350)
+
+#거북이 이동 경로
+def turtle_drive(t,a,b,x,y):
+    t.penup()
+    t.goto(a,b)
+    t.pendown()
+    t.goto(-50, -50)
+    t.lt(60)
+    t.fd(200)
+    t.goto(x,y)
+    print(turtle_distance(a,b,x,y))
+      
+#출발, 도착지점 거리계산함수
+def turtle_distance(a,b,x,y):
+    t_distace_square = (a-x) * (a-x) + (b-y) * (b-y)
+    t_distance = math.sqrt(t_distace_square)
+    return t_distance
+
+#프로그램실행   
+turtle_drive(t, -350, -350, 350, 350)
