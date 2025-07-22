8f7ba2e4fb46adf5dd9798735483f21432f395e9
ngyh-1002 <01049200297aa@gmail.com>
2025-07-22 10:14:58 +0900

    거북이 경로지정

--- turtle_robot.py
+++ turtle_robot.py
@@ -5,3 +5,11 @@ s = turtle.getscreen()
 
 #거북이 변수 지정
 t = turtle.Turtle()
+t.shapesize(3,3,3)
+t.shape('turtle')
+t.color("green", "blue")
+t.pensize(5)
+t.penup()
+t.goto(-350,-350)
+t.pendown()
+t.goto(350, 350)
