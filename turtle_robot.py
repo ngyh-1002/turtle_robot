a3f0147482b5bcc5e9d11d7ec8a9901afb77ef87
ngyh-1002 <01049200297aa@gmail.com>
2025-07-22 10:28:07 +0900

    장애물설치
    장애물 및 출발지점 도착지점 표시

--- turtle_robot.py
+++ turtle_robot.py
@@ -2,6 +2,7 @@ import turtle
 
 # 스크린 생성
 s = turtle.getscreen()
+s.screensize(400, 400)
 
 #거북이 변수 지정
 t = turtle.Turtle()
@@ -9,6 +10,27 @@ t.shapesize(3,3,3)
 t.shape('turtle')
 t.color("green", "blue")
 t.pensize(5)
+
+t.penup()
+t.goto(-350,-375)
+t.pendown()
+t.circle(50)
+
+t.penup()
+t.goto(325,325)
+t.pendown()
+for i in range(4):
+    t.fd(100)
+    t.lt(90)
+
+t.penup()
+t.goto(0,0)
+t.pendown()
+
+for i in range(3):
+    t.fd(100)
+    t.lt(120)
+    
 t.penup()
 t.goto(-350,-350)
 t.pendown()
